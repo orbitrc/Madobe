a3e3ed3f279934bef46447e8037a93b78eb17ff2
Yujŏnja <hardboiled65@gmail.com>
2022-04-17 15:07:53 +0900

    Add modules.

---
+++ src/madobedecoration.cpp
@@ -0,0 +1,9 @@
+#include "madobedecoration.h"
+
+#include <KPluginFactory>
+#include <KDecoration2/Decoration>
+
+K_PLUGIN_FACTORY_WITH_JSON(
+    MadobeDecorationFactory,
+    "madobe.json",
+)
---
+++ src/madobedecoration.h
@@ -0,0 +1,2 @@
+namespace madobe {
+}
