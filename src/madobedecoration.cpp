6747fa0dc4ac5f7c1f69992b02275feb94188bfe
Yujŏnja <hardboiled65@gmail.com>
2022-04-18 17:49:04 +0900

    Minimal build success.

--- src/madobedecoration.cpp
+++ src/madobedecoration.cpp
@@ -6,4 +6,24 @@
 K_PLUGIN_FACTORY_WITH_JSON(
     MadobeDecorationFactory,
     "madobe.json",
+    registerPlugin<madobe::Decoration>();
 )
+
+namespace madobe {
+    Decoration::Decoration(QObject *parent, const QVariantList& args)
+        : KDecoration2::Decoration(parent, args)
+    {
+        // TODO: Implement.
+    }
+
+    Decoration::~Decoration()
+    {
+    }
+
+    void Decoration::paint(QPainter *painter, const QRect& repaintRegion)
+    {
+        // TODO: Implement.
+    }
+}
+
+#include "madobedecoration.moc"
--- src/madobedecoration.h
+++ src/madobedecoration.h
@@ -1,2 +1,23 @@
+#ifndef _MADOBEDECORATION_H
+#define _MADOBEDECORATION_H
+
+#include <QVariant>
+
+#include <KDecoration2/Decoration>
+
 namespace madobe {
+    class Decoration : public KDecoration2::Decoration
+    {
+        Q_OBJECT
+    public:
+        explicit Decoration(QObject *parent = nullptr,
+                const QVariantList& args = QVariantList());
+
+        virtual ~Decoration();
+
+        void paint(QPainter *painter, const QRect& repaintRegion) override;
+    private:
+    };
 }
+
+#endif // _MADOBEDECORATION_H
